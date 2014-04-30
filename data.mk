e4de596eb7b227a19b3c61fc92e43db52efcaa09
Duncan McGreggor <duncan@cogitat.io>
2014-04-30 00:08:51 -0700

    Fixed lfetool subcommand.

--- data.mk
+++ data.mk
@@ -2,6 +2,6 @@ DB=company-name
 
 mnesia-shell: compile-no-deps
 	clear
-	@ERL_LIBS=$(shell lfetool info erllib) \
+	@ERL_LIBS=$(shell lfetool info erllibs) \
 	PATH=$(SCRIPT_PATH) \
 	lfe -pa $(TEST_OUT_DIR) -mnesia dir '"$(DB)"'
