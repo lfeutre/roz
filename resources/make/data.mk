5d92054714963592563ceec8aa66372f43953053
Duncan McGreggor <duncan@cogitat.io>
2014-05-03 00:46:05 -0700

    Added placeholders for using named nodes in the future.

--- resources/make/data.mk
+++ resources/make/data.mk
@@ -1,7 +1,9 @@
 DB=$(shell cat resources/config/company-name)
+#NODE=-sname roz
+NODE=
 
 mnesia-shell: compile-no-deps
 	clear
 	@ERL_LIBS=$(shell lfetool info erllibs) \
 	PATH=$(SCRIPT_PATH) \
-	lfe -pa $(TEST_OUT_DIR) -mnesia dir '"$(DB)"'
+	lfe -pa $(TEST_OUT_DIR) $(NODE) -mnesia dir '"$(DB)"'
