30197b05c267188f40e80cb54ff763eb3f8d6411
Duncan McGreggor <duncan@cogitat.io>
2014-05-03 20:22:42 -0700

    Split up targets for starting a shell.

--- resources/make/data.mk
+++ resources/make/data.mk
@@ -2,8 +2,11 @@ DB=$(shell cat resources/config/company-name)
 #NODE=-sname roz
 NODE=
 
-mnesia-shell: compile-no-deps
-	clear
-	@ERL_LIBS=$(shell lfetool info erllibs) \
+mnesia-shell-compile: compile-no-deps mnesia-shell
+
+mnesia-shell:
+	ERL_LIBS=$(shell lfetool info erllibs) \
 	PATH=$(SCRIPT_PATH) \
-	lfe -pa $(TEST_OUT_DIR) $(NODE) -mnesia dir '"$(DB)"' -run mnesia start
+	lfe -mnesia dir '"$(DB)"'
+
+.PHONY: mnesia-shell-compile mnesia-shell
